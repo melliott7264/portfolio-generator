dde966dea408174fcc08ab7412c1b872fe881a20
Mark Elliott <96553641+melliott7264@users.noreply.github.com>
2022-04-18 16:31:54 -0400

    added validation of required input

--- app.js
+++ app.js
@@ -17,15 +17,31 @@ const promptUser = () => {
         {
            type:    'input',
            name:    'name',
-           message: 'What is your name?'
+           message: 'What is your name? (Required)',
+           validate: nameInput => {
+               if (nameInput) {
+                   return true;
+               } else {
+                   console.log('Please enter your name!');
+                   return false;
+               }
+           }
         },
         {
             type:   'input',
             name:   'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub Username!');
+                    return false;
+                }
+            }
         },
         {
-            tupe:   'input',
+            type:   'input',
             name:   'about',
             message:    'Provide some information about yourself:'
         }
@@ -51,12 +67,28 @@ const promptProject = (portfolioData) => {
         {
             type:   'input',
             name:   'name',
-            message:    'What is the name of your project?'
+            message:    'What is the name of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project name!');
+                    return false;
+                }
+            }    
         },
         {
             type:   'input',
             name:   'description',
-            message:    'Provide a description of the project (Required)'
+            message:    'Provide a description of the project (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project description!');
+                    return false;
+                }
+            }
         },
         {
             type:   'checkbox',
@@ -67,7 +99,15 @@ const promptProject = (portfolioData) => {
         {
             type:   'input',
             name:   'link',
-            message:    'Enter the GitHub link to your project. (Required)'
+            message:    'Enter the GitHub link to your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the GitHub link to your project!');
+                    return false;
+                }
+            }
         },
         {
             type:   'confirm',
